597885f66b0df287bdf8f64b6460cf51b43c1d06
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-02 11:26:40 +0530

    Initial commit

---
+++ src/main/java/dev/ju/nextcart/NextcartApplication.java
@@ -0,0 +1,13 @@
+package dev.ju.nextcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextcartApplication.class, args);
+	}
+
+}
