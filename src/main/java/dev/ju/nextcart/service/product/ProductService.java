c09e1d97968c4f32a8a10f31f1aaa9ce5df2be5d
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-03 12:01:38 +0530

    Get product by ID service

---
+++ src/main/java/dev/ju/nextcart/service/product/ProductService.java
@@ -0,0 +1,73 @@
+package dev.ju.nextcart.service.product;
+
+import dev.ju.nextcart.exceptions.BadRequestException;
+import dev.ju.nextcart.model.Product;
+import dev.ju.nextcart.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService implements IProductService{
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new BadRequestException("Product with ID: "+id+" does not exist!"));
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        return;
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByCategory(Long categoryId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
+}
