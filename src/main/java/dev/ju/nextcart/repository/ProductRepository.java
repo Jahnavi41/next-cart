88216c84465f4c40582ff286f4ffbbcfc6337777
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-03 12:05:45 +0530

    Create Jpa method to fetch products by category name

--- src/main/java/dev/ju/nextcart/repository/ProductRepository.java
+++ src/main/java/dev/ju/nextcart/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package dev.ju.nextcart.repository;
 import dev.ju.nextcart.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryName(String categoryName);
 }
