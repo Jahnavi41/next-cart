8a36f6e7598963f0775c89f98371894ce4ada91d
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-03 12:02:43 +0530

    Get All products service

--- src/main/java/dev/ju/nextcart/service/product/ProductService.java
+++ src/main/java/dev/ju/nextcart/service/product/ProductService.java
@@ -40,7 +40,7 @@ public class ProductService implements IProductService{
 
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+        return productRepository.findAll();
     }
 
     @Override
