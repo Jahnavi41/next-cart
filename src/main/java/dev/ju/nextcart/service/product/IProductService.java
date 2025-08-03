2be403ccf012f806533d12f5d37871787ba2e519
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-03 12:01:12 +0530

    Added all required Product services

---
+++ src/main/java/dev/ju/nextcart/service/product/IProductService.java
@@ -0,0 +1,20 @@
+package dev.ju.nextcart.service.product;
+
+import dev.ju.nextcart.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct(Product product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product, Long productId);
+
+    List<Product> getAllProducts();
+    List<Product> getProductByCategory(Long categoryId);
+    List<Product> getProductByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+}
