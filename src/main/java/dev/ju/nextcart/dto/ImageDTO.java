39160dddf7d0b15d67be01538d1267a70b509981
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-04 17:38:39 +0530

    ImageDTO

---
+++ src/main/java/dev/ju/nextcart/dto/ImageDTO.java
@@ -0,0 +1,12 @@
+package dev.ju.nextcart.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImageDTO {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
+}
