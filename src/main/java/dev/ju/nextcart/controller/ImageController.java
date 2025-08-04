316c1d05c8120e7dd7faff8c9e80a5953b2cd347
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-04 21:35:20 +0530

    Add: Controller Image services (save, download)

--- src/main/java/dev/ju/nextcart/controller/ImageController.java
+++ src/main/java/dev/ju/nextcart/controller/ImageController.java
@@ -1,4 +1,53 @@
 package dev.ju.nextcart.controller;
 
+import dev.ju.nextcart.dto.ImageDTO;
+import dev.ju.nextcart.model.Image;
+import dev.ju.nextcart.response.ApiResponse;
+import dev.ju.nextcart.service.image.IImageService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/images")
 public class ImageController {
+    public static final HttpStatus INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR;
+    private final IImageService imageService;
+
+    public ImageController(IImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long productId) {
+        try {
+            List<ImageDTO> imageDTOS = imageService.saveImage(files, productId);
+            return ResponseEntity.ok(new ApiResponse("Images uploaded successfully!", imageDTOS));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Image uploading failed!", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.parseMediaType(image.getFileType()))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+                    .body(resource);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
