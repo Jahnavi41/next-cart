c209c4a00482ce2ab3f3f1122789c1837f4e2bc4
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-05 21:54:50 +0530

    Add: add controller

--- src/main/java/dev/ju/nextcart/controller/ProductController.java
+++ src/main/java/dev/ju/nextcart/controller/ProductController.java
@@ -2,14 +2,13 @@ package dev.ju.nextcart.controller;
 
 import dev.ju.nextcart.exceptions.BadRequestException;
 import dev.ju.nextcart.model.Product;
+import dev.ju.nextcart.request.AddProductRequest;
+import dev.ju.nextcart.request.UpdateProductRequest;
 import dev.ju.nextcart.response.ApiResponse;
 import dev.ju.nextcart.service.product.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +16,7 @@ import java.util.List;
 @RequestMapping("${api.prefix}/products")
 public class ProductController {
     public static final HttpStatus NOT_FOUND = HttpStatus.NOT_FOUND;
+    public static final HttpStatus INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR;
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -42,4 +42,16 @@ public class ProductController {
             return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), NOT_FOUND));
         }
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest product) {
+        try {
+            Product createdProduct = productService.addProduct(product);
+            return ResponseEntity.ok(new ApiResponse("Product created successfully!", createdProduct));
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
 }
