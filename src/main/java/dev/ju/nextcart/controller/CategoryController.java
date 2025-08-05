774dbefc75cb428f183c7733ff4093f1ac074f35
Jahnavi41 <jahnaviundela43@gmail.com>
2025-08-05 21:25:23 +0530

    Add: Category getAll controller

--- src/main/java/dev/ju/nextcart/controller/CategoryController.java
+++ src/main/java/dev/ju/nextcart/controller/CategoryController.java
@@ -1,4 +1,38 @@
 package dev.ju.nextcart.controller;
 
+import dev.ju.nextcart.exceptions.BadRequestException;
+import dev.ju.nextcart.model.Category;
+import dev.ju.nextcart.response.ApiResponse;
+import dev.ju.nextcart.service.category.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    public static final HttpStatus INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR;
+    public static final HttpStatus CONFLICT = HttpStatus.CONFLICT;
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/getAllCategories")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Categories found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Could not fetch all categories!", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+
 }
