b912ac8a8f1ba7c2bad476868c11075e848f5234
Jahnavi41 <jahnaviundela43@gmail.com>
2025-09-02 13:35:30 +0530

    Added clear and totalPrice controller

--- src/main/java/dev/ju/nextcart/controller/CartController.java
+++ src/main/java/dev/ju/nextcart/controller/CartController.java
@@ -4,12 +4,12 @@ import dev.ju.nextcart.exceptions.BadRequestException;
 import dev.ju.nextcart.model.Cart;
 import dev.ju.nextcart.response.ApiResponse;
 import dev.ju.nextcart.service.cart.ICartService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("${api.prefix}/cart")
@@ -21,7 +21,7 @@ public class CartController {
         this.cartService = cartService;
     }
 
-    @GetMapping("{cartId}/my-cart")
+    @GetMapping("/{cartId}/my-cart")
     public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
         try {
             Cart cart = cartService.getCart(cartId);
@@ -29,5 +29,17 @@ public class CartController {
         } catch (BadRequestException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
         }
+    }
+
+    @DeleteMapping("/{cartId}/clear")
+    public ResponseEntity<ApiResponse> clearCart(@PathVariable Long cartId) {
+        cartService.clearCart(cartId);
+        return ResponseEntity.ok( new ApiResponse("Clear Cart success!", null));
+    }
+
+    @GetMapping("/{cartId}/cart/totalPrice")
+    public ResponseEntity<ApiResponse> getTotalAmount(@PathVariable Long cartId) {
+        BigDecimal totalPrice = cartService.getTotalPrice(cartId);
+        return ResponseEntity.ok(new ApiResponse("Total Price!", totalPrice));
     }
 }
