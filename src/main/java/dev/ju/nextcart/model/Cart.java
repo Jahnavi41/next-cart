bd2e41010361c8658ba301c5124cb69d1e582b5e
Jahnavi41 <jahnaviundela43@gmail.com>
2025-09-02 11:43:48 +0530

    Cart and CartItem entities

---
+++ src/main/java/dev/ju/nextcart/model/Cart.java
@@ -0,0 +1,26 @@
+package dev.ju.nextcart.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    private Long id;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> cartItems;
+}
---
+++ src/main/java/dev/ju/nextcart/model/CartItem.java
@@ -0,0 +1,27 @@
+package dev.ju.nextcart.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public void setTotalPrice() {
+        this.totalPrice = this.unitPrice.multiply(new BigDecimal(quantity));
+    }
+}
