409eae6fc634c852777a1f90a25f6e73ef8dc851
Jahnavi41 <jahnaviundela43@gmail.com>
2025-09-02 13:24:30 +0530

    Added Cart Repositories

---
+++ src/main/java/dev/ju/nextcart/repository/CartItemRepository.java
@@ -0,0 +1,8 @@
+package dev.ju.nextcart.repository;
+
+import dev.ju.nextcart.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    void deleteAllByCartId(Long id);
+}
