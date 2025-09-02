6f859d9a99e5b6210d98a01f4bb4b83018dc971c
Jahnavi41 <jahnaviundela43@gmail.com>
2025-09-02 21:42:52 +0530

    CartItemController

---
+++ src/main/java/dev/ju/nextcart/controller/CartItemController.java
@@ -0,0 +1,56 @@
+package dev.ju.nextcart.controller;
+
+import dev.ju.nextcart.exceptions.BadRequestException;
+import dev.ju.nextcart.response.ApiResponse;
+import dev.ju.nextcart.service.cart.ICartItemService;
+import dev.ju.nextcart.service.cart.ICartService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("${api.prefix}/cartItems")
+public class CartItemController {
+    public static final HttpStatus NOT_FOUND = HttpStatus.NOT_FOUND;
+    private final ICartItemService cartItemService;
+    private final ICartService cartService;
+
+    public CartItemController(ICartItemService cartItemService, ICartService cartService) {
+        this.cartItemService = cartItemService;
+        this.cartService = cartService;
+    }
+
+    @PostMapping("/item/add")
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long cartId, @RequestParam Long productId, @RequestParam Integer quantity) {
+        try {
+            if(cartId == null) {
+                cartId = cartService.initializeNewCart();
+            }
+            cartItemService.addCartItem(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Add Item Success", null));
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/cart/{cartId}/item/{itemId}/remove")
+    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable Long cartId, @PathVariable Long itemId) {
+        try {
+            cartItemService.removeItemFromCart(cartId, itemId);
+            return ResponseEntity.ok(new ApiResponse("Remove Item Success", null));
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/cart/{cartId}/item/{itemId}/update")
+    public  ResponseEntity<ApiResponse> updateItemQuantity(@PathVariable Long cartId, @PathVariable Long itemId, @RequestParam Integer quantity) {
+        try {
+            cartItemService.updateItemQuantity(cartId, itemId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Update Item Success", null));
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+}
