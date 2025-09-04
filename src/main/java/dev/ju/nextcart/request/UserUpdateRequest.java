f66f3131d8811e85c9691fd7b3c802511bf7cb63
Jahnavi41 <jahnaviundela43@gmail.com>
2025-09-04 22:41:26 +0530

    User create and update requests

---
+++ src/main/java/dev/ju/nextcart/request/UserUpdateRequest.java
@@ -0,0 +1,15 @@
+package dev.ju.nextcart.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateRequest {
+    private String firstName;
+    private String lastName;
+}
